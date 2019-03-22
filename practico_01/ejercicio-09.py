7b9dd9359a554f93922cd42ac21299f05b0f020e
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2019-03-22 16:07:48 -0300

    Update ejercicio-09.py
    v1.1 Nicolas Bologna

--- practico_01/ejercicio-09.py
+++ practico_01/ejercicio-09.py
@@ -11,6 +11,6 @@ def es_palindromo(palabra):
 
 assert (es_palindromo("arenera")) == True
 assert (es_palindromo("areneras")) == False
-assert (es_palindromo("radar")) == False
-assert (es_palindromo("ojo")) == False
-assert (es_palindromo("salas")) == False
+assert (es_palindromo("radar")) == True
+assert (es_palindromo("ojo")) == True
+assert (es_palindromo("salas")) == True
