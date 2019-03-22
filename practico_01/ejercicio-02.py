a1480233258218a8cd3cf07e9f05b6f3b12e349f
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2019-03-22 14:10:24 -0300

    v1.0 - Nicolás Bologna

--- practico_01/ejercicio-02.py
+++ practico_01/ejercicio-02.py
@@ -2,4 +2,22 @@
 
 
 def mayor(a, b, c):
-    pass
+    if a > b or a == b:
+        if a > c or a == c:
+            return a
+        else:
+            return c
+    else:
+        if b > c:
+            return b
+        else:
+            return c
+
+assert mayor(1,2,3) == 3
+assert mayor(1,3,2) == 3
+assert mayor(3,1,2) == 3
+assert mayor(2,3,3) == 3
+assert mayor(3,2,3) == 3
+assert mayor(3,3,2) == 3
+assert mayor(3,3,3) == 3
+
