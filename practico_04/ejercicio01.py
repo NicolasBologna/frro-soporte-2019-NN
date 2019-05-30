c11dce25c816a3d3b46c163b6cd8285ba383dd97
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2019-05-30 03:37:37 -0300

    v0.1 Nicolas Bologna

--- practico_04/ejercicio01.py
+++ practico_04/ejercicio01.py
@@ -2,4 +2,91 @@
 ## Y 4 botones de operaciones para las operaciones respectivas + , - , * , / ,
 ## al cliquearlos muestre el resultado de aplicar el operador respectivo en los V1 y V2 . 
  
+import tkinter as tk
+
+calculadora = tk.Tk()
+calculadora.title('Calculadora')
+
+
+def pide_cerrar():
+    calculadora.destroy()
+    calculadora.quit()
+calculadora.protocol("WM_DELETE_WINDOW", pide_cerrar)
+
+
+label = tk.Label(calculadora, text="Primer Operando: ")
+label2 = tk.Label(calculadora, text="Segundo Operando: ")
+label.grid(column=1, row=1, padx=(50,50), pady=(10,10))
+label2.grid(column=2, row=1, padx=(40,40), pady=(10,5))
+evar1 = tk.DoubleVar()
+evar1.set('')
+evar2 = tk.DoubleVar()
+evar2.set('')
+res = tk.DoubleVar()
+entry1 = tk.Entry(calculadora,textvariable = evar1)
+entry2 = tk.Entry(calculadora,textvariable = evar2)
+entry1.grid(column=1, row=2)
+entry2.grid(column=2, row=2)
+
+btn_mas = tk.Button(calculadora, text="+")
+def sumar(event):
+    try:
+        res.set(evar1.get() + evar2.get())
+        evar1.set('')
+        evar2.set('')
+        print(res.get())
+        lbl_res['text'] = res.get() #permite cambiar atributos mientras está corriendo
+    except:
+         lbl_res['text'] = 'Error'
+btn_mas.bind("<Button-1>", sumar)
+
+btn_menos = tk.Button(calculadora, text="-")
+def restar(event):
+    try:
+        res.set(evar1.get() - evar2.get())
+        evar1.set('')
+        evar2.set('')
+        print(res.get())
+        lbl_res['text'] = res.get()
+    except:
+         lbl_res['text'] = 'Error'
+btn_menos.bind("<Button-1>", restar)
+
+
+btn_por = tk.Button(calculadora, text="*")
+def multiplicar(event):
+    try:
+        res.set(evar1.get() * evar2.get())
+        evar1.set('')
+        evar2.set('')
+        print(res.get())
+        lbl_res['text'] = res.get()
+    except:
+         lbl_res['text'] = 'Error'
+btn_por.bind("<Button-1>", multiplicar)
+
+
+btn_div = tk.Button(calculadora, text="/")
+def dividir(event):
+    try:
+        res.set(evar1.get() / evar2.get())
+        evar1.set('')
+        evar2.set('')
+        print(res.get())
+        lbl_res['text'] = res.get()
+    except:
+         lbl_res['text'] = 'Error'
+btn_div.bind("<Button-1>", dividir)
+
+lbl_res = tk.Label(calculadora, text=res.get())
+
+btn_mas.grid(column=3, row=2, padx=(5,5), pady=(10,5))
+btn_menos.grid(column=4, row=2, padx=(5,5), pady=(10,5))
+btn_por.grid(column=5, row=2, padx=(5,5), pady=(10,5))
+btn_div.grid(column=6, row=2, padx=(5,5), pady=(10,5))
+lbl_res.grid(column=3, row=3, padx=(5,5), pady=(10,5))
+
+
+calculadora.mainloop()
+
 
--- practico_04/ejercicio03.py
+++ practico_04/ejercicio03.py
@@ -1,2 +1,33 @@
 ## 3 Ejercicio Crear un Formulario que usando el control Treeview muestre la una lista con los nombre de
 ## Ciudades Argentinas y su código postal ( por lo menos 5 ciudades ) . 
+
+import tkinter as tk
+from tkinter import ttk
+
+
+
+class Application(ttk.Frame):
+
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        main_window.title("Formulario")
+
+        self.treeview = ttk.Treeview(self)
+        item = self.treeview.insert("", tk.END, text="Rosario")
+        self.treeview.insert(item, tk.END, text="2000")
+        item = self.treeview.insert("", tk.END, text="Roldan")
+        self.treeview.insert(item, tk.END, text="2134")
+        item = self.treeview.insert("", tk.END, text="Funes")
+        self.treeview.insert(item, tk.END, text="2132")
+        item = self.treeview.insert("", tk.END, text="San Lorenzo")
+        self.treeview.insert(item, tk.END, text="2200")
+        item = self.treeview.insert("", tk.END, text="Villa Gobernador Gálvez")
+        self.treeview.insert(item, tk.END, text="2124")
+        self.treeview.pack()
+
+        self.pack()
+
+
+main_window = tk.Tk()
+app = Application(main_window)
+app.mainloop()
